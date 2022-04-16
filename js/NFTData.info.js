d41f4d5cbd8cbb0b02a9092281cb5836a78efa91
Bharat Bhalla <81501575+BharatXBhalla@users.noreply.github.com>
2022-04-16 17:12:43 +0530

    base

---
+++ js/NFTData.info.js
@@ -0,0 +1,31 @@
+const PaintingInfo=[
+    {
+        description:"Lost in Eye of Eye is a illusionary art to trick you to think twice over difficulties that one faces in this world.",
+        imagename:"1.jpg",
+        buylink:"https://opensea.io/assets/0x495f947276749ce646f68ac8c248420045cb7b5e/39578265878442030230169694892445739814183531419535469667928704318564095492097",
+        name:"Eye of Eye"
+    },
+    {
+        description:`Ever Travelled on a long highway.
+        Horizon during dusk is a hope to a new beginning.
+        Although one can truly see horizon near oceans but time has changed and we now see horizon on our Urban Roads.
+        At some point we will reach our final goal of immortality.`,
+        imagename:"2.jpg",
+        buylink:"https://opensea.io/assets/0x495f947276749ce646f68ac8c248420045cb7b5e/39578265878442030230169694892445739814183531419535469667928704321862630375425",
+        name:"Lost in Urban Horizon"
+    },
+    {
+        description:"Doesn't matter how many times life drag you down never stop like the waves of the ocean.",
+        imagename:"ocean website.jpg",
+        buylink:"https://opensea.io/assets/0x495f947276749ce646f68ac8c248420045cb7b5e/39578265878442030230169694892445739814183531419535469667928704340554328047617",
+        name:"Lost in Ocean"
+    }
+
+]
+
+// {
+//     description:
+//     imagename:
+//     buylink:
+//     name:
+// }
